fa27e0bd62765867a8f45d781bdaf8b36c77a0ab
Sam <samshum90@gmail.com>
2020-11-07 16:34:01 +0000

    changed favicon

---
+++ build/precache-manifest.39e48ff957c390bee961bd2a0f36ba0a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ec8fb57b81cbf1958b72ff09f23d94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b906b2f068970ffdffc6",
+    "url": "/static/css/main.ee956a05.chunk.css"
+  },
+  {
+    "revision": "369cf8ecc301a6d1e7b9",
+    "url": "/static/js/2.d69e95ff.chunk.js"
+  },
+  {
+    "revision": "16e72e8a7b17b4817847b2ca58cd85b3",
+    "url": "/static/js/2.d69e95ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b906b2f068970ffdffc6",
+    "url": "/static/js/main.2cb1e676.chunk.js"
+  },
+  {
+    "revision": "b241c277af25b6d4687c",
+    "url": "/static/js/runtime-main.7a88e329.js"
+  },
+  {
+    "revision": "44153d4039e5d9650d716ab019ea791f",
+    "url": "/static/media/activities.44153d40.jpg"
+  },
+  {
+    "revision": "ded9f77ffc3b70d316f6db716dd9b3e0",
+    "url": "/static/media/booking.ded9f77f.jpg"
+  },
+  {
+    "revision": "ef874ee47dd3315d0a21becf1d72b523",
+    "url": "/static/media/guest.ef874ee4.jpg"
+  },
+  {
+    "revision": "d7ec623490b2b4cfe5e1e953650d458b",
+    "url": "/static/media/landing.d7ec6234.jpg"
+  },
+  {
+    "revision": "f6749a4c80a3b748cef37bda4c580a36",
+    "url": "/static/media/register.f6749a4c.jpg"
+  },
+  {
+    "revision": "f692034941e29ae7d2269c5dfc9c597a",
+    "url": "/static/media/reservation.f6920349.jpg"
+  },
+  {
+    "revision": "8b9a8162cc51a848ba33fea5f8242522",
+    "url": "/static/media/room.8b9a8162.jpg"
+  },
+  {
+    "revision": "7b8fe04417ac1c12d6f1e8229b51b657",
+    "url": "/static/media/staff.7b8fe044.jpg"
+  }
+]);
